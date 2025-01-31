24ebc1b36d6d943476f5ab059b6d252e186d2628
RidgelineFire <99427032+dcolbourne@users.noreply.github.com>
2025-01-31 10:20:17 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+function diceSelectLeft() {
+    var randomNumber1 = Math.floor(Math.random() * 6 + 1);
+    var randomDice = "dice" + randomNumber1 + ".png";
+    document.querySelector(".img1").setAttribute("src", "./images/" + randomDice);
+}
+
+function diceSelectRight() {
+    var randomNumber1 = Math.floor(Math.random() * 6 + 1 );
+    var randomDice = "dice" + randomNumber1 + ".png";
+    document.querySelector(".img2").setAttribute("src", "./images/" + randomDice);
+}
+
+function winTitle() {
+    var oneWins = "Player 1 Wins!"
+    var twoWins = "Player 2 Wins!"
+    
+    if (document.querySelector(".img1").getAttribute("src")[13] > document.querySelector(".img2").getAttribute("src")[13]) {
+        document.querySelector("h1").innerText = "Player 1 Wins!";
+    } else {
+        document.querySelector("h1").innerText = "Player 2 Wins!";
+    }
+}
+
+window.onload = function() {
+    diceSelectLeft();
+    diceSelectRight();
+    winTitle();
+  }
